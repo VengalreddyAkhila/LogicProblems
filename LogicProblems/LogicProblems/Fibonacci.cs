a30bfecd42a74acb96c60ef8b7c7e262a8cd6627
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-28 08:02:28 +0530

    Refactor:logic problems

---
+++ LogicProblems/LogicProblems/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicProblems
+{
+    public class Fibonacci
+    {
+        public void Fibb(int len)
+        {
+           
+            int a = 0, b = 1, c ;
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.WriteLine("c", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
---
+++ LogicProblems/LogicProblems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogicProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logic programs");
+
+            Console.WriteLine("Enter a choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Fibb(5);
+                    break;
+                case 2:
+
+                    PerfectNumber perfectnumber = new PerfectNumber();
+                    perfectnumber.PerfectNum();
+                    break;
+            }
+           
+        }
+    }
+}
